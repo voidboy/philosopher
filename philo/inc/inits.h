0520946eaf81f043a6e1eafe86acb17d1e389bef
Aurelien Clerac <aclerac@c1r1p8.42nice.fr>
2021-07-12 09:51:35 +0200

    norminette

--- philo/inc/inits.h
+++ philo/inc/inits.h
@@ -1,11 +1,11 @@
 #ifndef INITS_H
 # define INITS_H
 int		ft_fail_philo(int i, pthread_t *threads, t_philo *philo,
-		pthread_mutex_t *forks);
+			pthread_mutex_t *forks);
 void	ft_free_philo(pthread_t *threads, t_philo *philo,
-		pthread_mutex_t *forks, t_opts *options);
+			pthread_mutex_t *forks, t_opts *options);
 int		ft_init_philo(t_opts *options, pthread_t *threads, t_philo *philo,
-		pthread_mutex_t *forks);
+			pthread_mutex_t *forks);
 int		ft_alloc(t_opts *options, pthread_t **threads, t_philo **philo,
-		pthread_mutex_t **forks);
+			pthread_mutex_t **forks);
 #endif
